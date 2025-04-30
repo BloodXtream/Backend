fc1916d89b406bc1c5dd84f219d2b5553c607e47
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-04-30 14:38:39 +0530

    Delete module_pt directory

--- module_pt/01_app.js
+++
@@ -1,8 +0,0 @@
-// console.log(global)
-// global.console.log("hello")
-
-const math = require('./math')
-console.log(math.sum(1, 2))
-console.log(math.sub(1, 2))
-console.log(math.multi(1, 2))
-console.log(math.div(1, 2))
