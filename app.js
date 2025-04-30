6a58b5225d07dc1781e04dd9c4b74a4c1c723c88
BloodXtream <pragyanamtiwarii@gmail.com>
2025-04-30 14:30:13 +0530

    upload todo list project to main branch

---
+++ app.js
@@ -0,0 +1,41 @@
+import readline from "readline"
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const todos = []
+
+const showMenu = () => {
+    console.log('\n1: Add a Task')
+    console.log('2: View Tasks')
+    console.log('3: Exit')
+    rl.question("Choose an option: ", handleInput)
+}
+
+const handleInput = (option) => {
+    if (option === "1") {
+        rl.question("Enter a task: ", (task) => {
+            todos.push(task)
+            console.log(`Task Added: ${task}`)
+            showMenu()
+        })
+    }
+    else if (option === "2") {
+        console.log("\n Your Todo List: ")
+        todos.forEach((task, index) => {
+            console.log(`${index + 1}: ${task}`)
+        })
+        showMenu()
+    }
+    else if (option === "3") {
+        console.log("\n GoodByeee...👋🏻")
+        rl.close()
+    }
+    else {
+        console.log("Invalid Option Try Agian")
+        showMenu()
+    }
+}
+showMenu()
